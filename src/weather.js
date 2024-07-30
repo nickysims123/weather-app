7d55e6f34de5b3314faee4f0e3100eabf9e31a9c
Nicholas Sima <nick.sima@outlook.com>
2024-07-30 15:36:04 -0400

    Testing to see if dependencies were created correctly

--- src/weather.js
+++ src/weather.js
@@ -1,2 +1,8 @@
 // This file retrieves our desired data from visualcrossing's
 // weatherAPI. Exported and bundled using webpack
+
+const getWeather = async (city) => {
+    // creates url request based on city input
+    const url = 'https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/' + city + '?unitGroup=us&key=A9TGYA5G4HTQDAMUVNV8QVDNX&contentType=json';
+    const weather = await fetch(url);
+}
